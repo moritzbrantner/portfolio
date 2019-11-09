aa74e7c5bb34abd5a267dfc0074e2532ea2f11c5
Nuzee <Nuzee@protonmail.com>
2019-11-09 03:08:55 +0100

    part 3

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+// Select DOM items
+const menuButton = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set initial state of menu
+let showMenu = false;
+
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuButton.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+    } else {
+        menuButton.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+    }
+
+    showMenu = !showMenu;
+}
