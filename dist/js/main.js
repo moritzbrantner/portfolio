0fee883c9644978b23ab990bd0dacb92761ecb94
Nuzee <Nuzee@protonmail.com>
2019-11-09 14:35:59 +0100

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,13 +17,15 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
     } else {
         menuButton.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-    }
 
-    showMenu = !showMenu;
+        showMenu = false;
+    }
 }
